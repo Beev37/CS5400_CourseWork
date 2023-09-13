deece96b7264909b7dec04026e5786ea2b1a8d7f
Brendan Varilek <b@Brendans-MacBook-Air.local>
2023-09-13 09:03:44 -0700

    made repo added folders and files to repo

---
+++ HW1/hw1_familyTree.pl
@@ -0,0 +1,19 @@
+
+
+
+
+father(Dad, Child) :- parent(Dad, Child), male(Dad).
+mother(Mom, Child) :- parent(Mom, Child), female(Mom).
+
+sibling(Sibling1, Sibling2):-
+  parent(Parent, Sibling1),
+  parent(Parent, Sibling2),
+  Sibling1 \= Sibling2.
+
+brother(Brother, Sib) :-
+  sibling(Brother, Sib),
+  male(Brother).
+
+sister(Sister, Sib) :-
+  sibling(Sister, Sib),
+  female(Sister).
