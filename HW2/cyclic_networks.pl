1ceef66d8a0ccdf2d32376539a53e97c0986d32b
Brendan Varilek <b@Brendans-MacBook-Air.local>
2023-09-23 19:37:57 -0700

    finished exercise 2. need to wrap up exercise 1. having trouble understanding base case

---
+++ HW2/cyclic_networks.pl
@@ -0,0 +1,5 @@
+:-['cluster_data'].
+
+%cyclic(Cluster) :-
+  % TODO: define cyclic/1 to
+  % recognize cyclic clusters
--- HW2/exerciseTesting.pl
+++ HW2/exerciseTesting.pl
@@ -52,8 +52,77 @@ crossword(Vword1,Vword2,Vword3,Hword1,Hword2,Hword3) :-
 
 % Canvas Exercise: 1
 
+neighbor_left(clock, rocket).
+neighbor_left(rocket, guitar).
+neighbor_left(guitar, phone).
+
+neighbor_top(paperclip, clock).
+neighbor_top(scissors, guitar).
+
+neighbor_right(rocket, clock).
+neighbor_right(guitar, rocket).
+neighbor_right(phone, guitar).
+
+neighbor_bottom(clock, paperclip).
+neighbor_bottom(guitar, scissors).
+
+
+left_of(X,Y):- neighbor_left(X,Y).
+left_of(X,Y):- (neighbor_left(X,Z); neighbor_bottom(X,Z); neighbor_top(X,Z)), left_of(Z,Y).
+%left_of(paperclip, guitar) --> true
+
+above(X,Y):- neighbor_top(X,Y).
+above(X,Y):- neighbor_top(X,Z), (left_of(Z,Y); right_of(Z,Y)).
+
+right_of(X,Y):- neighbor_right(X,Y).
+right_of(X,Y):- (neighbor_right(X,Z); neighbor_bottom(X,Z); neighbor_top(X,Z)), right_of(Z,Y).
+%right_of(scissors, clock) --> true
+
+below(X,Y):- neighbor_bottom(X,Y).
+%below(X,Y):- neight
+
+
 % ______________________________________________________________
 
 % Canvas Exercise: 2
+
+link(m, w, 1).
+link(w, d, 1).
+link(d, y, 1).
+link(y, o, 1).
+link(o, s, 1).
+link(s, m, 1).
+
+link(i, c, 2).
+
+link(n, f, 3).
+link(f, j, 3).
+
+link(l, u, 4).
+link(u, e, 4).
+link(e, k, 4).
+link(k, p, 4).
+
+link(v, a, 5).
+link(a, h, 5).
+link(h, v, 5).
+
+link(b, z, 6).
+link(z, g, 6).
+link(g, x, 6).
+link(x, q, 6).
+link(q, r, 6).
+link(r, t, 6).
+link(t, b, 6).
+
+:-['cluster_data'].
+
+path(X,Y,C) :- link(X,Y,C).
+path(X,Y,C) :- link(X,Z,C), path(Z,Y,C).
+
+cyclic(Cluster) :-
+        path(X,X,Cluster).
+  % TODO: define cyclic/1 to
+  % recognize cyclic clusters
 
 % ______________________________________________________________
