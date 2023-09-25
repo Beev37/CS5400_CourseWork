0a42a0f05d2fdb3857376705d8e233359e31af3d
Brendan Varilek <b@Brendans-MacBook-Air.local>
2023-09-25 15:54:13 -0700

    getting rid of git notification

--- HW2/canvasExercise1.pl
+++ HW2/canvasExercise1.pl
@@ -18,23 +18,23 @@ neighbor_bottom(guitar, scissors).
 % Below 2 lines are my attempts at describing left_of() that can determine whether an object is to the left of another
 % regardless of what row they are located in. However, this implementation results in an infinite loop after finding
 % the initial answer. This loop prevents it from being useful to both "above()" and "below()".
-        % left_of(X,Y):- neighbor_left(X,Y).
-        % left_of(X,Y):- (neighbor_left(X,Z); neighbor_bottom(X,Z); neighbor_top(X,Z)), left_of(Z,Y).
+        left_of(X,Y):- neighbor_left(X,Y).
+        left_of(X,Y):- (neighbor_left(X,Z); neighbor_bottom(X,Z); neighbor_top(X,Z)), left_of(Z,Y).
 
-left_of(X,Y):- neighbor_left(X,Y).
-left_of(X,Y):- (neighbor_left(X,Z); neighbor_top(X,Z)), left_of(Z,Y).
+% left_of(X,Y):- neighbor_left(X,Y).
+% left_of(X,Y):- (neighbor_left(X,Z); neighbor_top(X,Z)), left_of(Z,Y).
 
 % Below 2 lines are my attempts at describing right_of() that can determine whether an object is to the right of another
 % regardless of what row they are located in. However, this implementation results in an infinite loop after finding
 % the initial answer. This loop prevents it from being useful to both "above()" and "below()".
-        % right_of(X,Y):- neighbor_right(X,Y).
-        % right_of(X,Y):- (neighbor_right(X,Z); neighbor_bottom(X,Z); neighbor_top(X,Z)), right_of(Z,Y).
+        right_of(X,Y):- neighbor_right(X,Y).
+        right_of(X,Y):- (neighbor_right(X,Z); neighbor_bottom(X,Z); neighbor_top(X,Z)), right_of(Z,Y).
 
-right_of(X,Y):- neighbor_right(X,Y).
-right_of(X,Y):- (neighbor_right(X,Z); neighbor_top(X,Z)), right_of(Z,Y).
+% right_of(X,Y):- neighbor_right(X,Y).
+% right_of(X,Y):- (neighbor_right(X,Z); neighbor_top(X,Z)), right_of(Z,Y).
 
 above(X,Y):- neighbor_top(X,Y).
 above(X,Y):- neighbor_top(X,Z), (left_of(Z,Y); right_of(Z,Y)).
 
 below(X,Y):- neighbor_bottom(X,Y).
-below(X,Y):- neighbor_bottom(X,Z), (left_of(Z,Y); right_of(Z,Y)).
+below(X,Y):- neighbor_bottom(X,Z), (left_of(Z,Y); right_of(Z,Y)). 
