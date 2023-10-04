ff8bac66776e426b1b303e56488ad8144159057c
Brendan Varilek <b@Brendans-MacBook-Air.local>
2023-10-03 23:21:57 -0700

    finished ex 2. start ex 3.

--- HW4/hw4.pl
+++ HW4/hw4.pl
@@ -11,14 +11,12 @@ substitute(X, Y, [H|T1], [H|T2]):-
 % Exercise 2: adjacent(X, Y, Zs)
 
 adjacent(_, _, []).
-adjacent(_, _, [_]).
 adjacent(X, Y, [X, Y | T]):-
     adjacent(X, Y, T).
 adjacent(X, Y, [Y, X | T]):-
     adjacent(X, Y, T).
-adjacent(X, Y, [H1, H2 | T]):- %issue happening here
-    X \= H1, X \= H2,
-    Y \= H1, Y \= H2,
+adjacent(X, Y, [H|T]):- %issue happening here 
+    X \= H, Y \= H,
     adjacent(X, Y, T).
 
 %_______________________________________________________________________________
