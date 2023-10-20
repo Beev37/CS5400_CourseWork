5a821144d2cbe0f423d2d256880fd3889757eef6
Brendan Varilek <b@Brendans-MacBook-Air.local>
2023-10-20 15:40:11 -0700

    finished hw6

--- HW6/NetworkTraversalStarter/networkTraversal.pl
+++ HW6/NetworkTraversalStarter/networkTraversal.pl
@@ -29,37 +29,42 @@ writeOut([H|T]) :-
 % other helper predicates as well
 % BEGIN CHANGES
 
+
 % red_edge_sequence/1
+% test1:-
+%   ['testNet1.pl'],
+%   red_edge_sequence([b,d,f,h,k]),
+%   red_edge_sequence([b,d,f,h,i,k]),
+%   red_edge_sequence([b,d,i,k]).
 
-test1:-
-  ['testNet1.pl'],
-  red_edge_sequence([b,d,f,h,k]),
-  red_edge_sequence([b,d,f,h,i,k]),
-  red_edge_sequence([b,d,i,k]).
+red_edge_sequence([H|T]):-
+  start_state(H),
+  rd_edg_sqnc([H|T]).
 
-red_edge_sequence([H|[]]):-
+rd_edg_sqnc([H|[]]):-
   end_state(H).
 
-red_edge_sequence([H1, H2|T]):-
+rd_edg_sqnc([H1, H2|T]):-
   trans(H1, H2, red),
-  red_edge_sequence([H2|T]).
+  rd_edg_sqnc([H2|T]).
+
 
 % alternating_edge_sequence/1
+% test2:-
+%   ['testNet1.pl'],
+%   alternating_edge_sequence([b,d,f,h,j]),
+%   alternating_edge_sequence([a,f,h,j]),
+%   alternating_edge_sequence([a,d,f,g,j]),
+%   alternating_edge_sequence([a,d,e,i,k]),
+%   alternating_edge_sequence([b,d,f,g,j]),
+%   alternating_edge_sequence([b,d,e,i,k]).
 
-test2:-
-  ['testNet1.pl'],
-  alternating_edge_sequence([b,d,f,h,j]),
-  alternating_edge_sequence([a,f,h,j]),
-  alternating_edge_sequence([a,d,f,g,j]),
-  alternating_edge_sequence([a,d,e,i,k]),
-  alternating_edge_sequence([b,d,f,g,j]),
-  alternating_edge_sequence([b,d,e,i,k]).
-
-alternating_edge_sequence(List):-
+alternating_edge_sequence([H|T]):-
+  start_state(H),
   (
-    alt_edge_seq(List, green)
+    alt_edge_seq([H|T], green)
     ;
-    alt_edge_seq(List, red)
+    alt_edge_seq([H|T], red)
   ).
 
 alt_edge_seq([H|[]], _):-
@@ -72,17 +77,17 @@ alt_edge_seq([H1, H2|T], Color):-
 
 
 % matching_edge_state_sequence/1
+% test3:-
+%   ['testNet1.pl'],
+%   matching_edge_state_sequence([a,f,g,h,j]),
+%   matching_edge_state_sequence([b,d,i,k]).
 
-test3:-
-  ['testNet1.pl'],
-  matching_edge_state_sequence([a,f,g,h,j]),
-  matching_edge_state_sequence([b,d,i,k]).
-
-matching_edge_state_sequence(List):-
+matching_edge_state_sequence([H|T]):-
+  start_state(H),
   (
-    mtch_edg_stt_seq(List, red)
+    mtch_edg_stt_seq([H|T], red)
     ;
-    mtch_edg_stt_seq(List, green)
+    mtch_edg_stt_seq([H|T], green)
   ).
 
 mtch_edg_stt_seq([H|[]], _):-
