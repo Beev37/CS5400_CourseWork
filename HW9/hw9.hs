d36cf6554e8c6c76af9eb7019f4cf32fec2c7efb
Beev37 <brendanvarilek@gmail.com>
2023-11-21 17:18:39 -0800

    added problem questions to hw file. starting ex 4

--- HW9/hw9.hs
+++ HW9/hw9.hs
@@ -38,3 +38,33 @@ dec2int = foldl (\x y -> 10*x+y) 0
 base2int :: Int -> String -> Int
 base2int b = foldl (\x y -> b*x+ digitToInt y) 0
 
+-- Exercise 4: Modify the Caesar cipher program to also handle upper-case letters. You should assume that the distribution of upper case letters is identical to the corresponding distribution of lower-case letters (i.e 'A' has the same probability as 'a'). Capital letters will be encoded analogously to their lower-case equivalents. Thus if 'h' is encoded as 'm' (as in the example below) 'H' will be encoded as 'M'.
+
+
+-- Exercise 5: Declare a data type 'Tree as follows:
+data Tree = Leaf Int | Node Tree Int Tree
+-- Write a function 'sumtree' that traverses the tree and returns the sum of the integer values of its nodes. For example, a call to 'sumtree' with a tree with three leaves and two non-terminal nodes with the following values:
+        -- Tree:
+        --     5
+        -- 5       2
+        --       5   3
+-- should result in the output '20', as shown:
+-- i> sumtree (Node (Leaf 5) 5 (Node (Leaf 5) 2 (Laf 3)))
+-- o> 20
+
+
+-- Exercise 6: Declare a data type 'Tree2' as follows:
+data Tree2 = Leaf2 Int | Node2 Tree2 Tree2
+-- Consider the tree to be balanced if the number of leaves in the left and right subtree of every node differs by at most one, with leaves themselves being trivially balanced. Define a function 
+-- 'balanced :: Tree2 -> Bool' that decides if a tree is balanced or not. Hint: first define a function that returns the number of leaves in a tree.
+
+balanced :: Tree2 -> Bool
+
+
+-- Exercise 7: In preparation for working on the Bert Bos puzzle in Haskell, it would be good to establish how to get a list of possible click lists. (The rest of the Bert Bos puzzle can be solved by figuring out how to filter this list down to only the successful click lists).
+-- You can generate a list of all possible "click" and "noclick" strings using two built-in functions that are both available in Prelude: the 'replicate' function and the 'sequence' function. Using these two functions, write a function 'clicklists :: Int -> [[[Char]]]' that takes an integer and outputs a list of lists of strings representing clicks and non-clicks.
+-- The documentation on the 'sequence' function may be a little bit confusing. When applied to an N-length list of lists, 'sequence' will yield a list of N element lists, where each of the elements are drawn from the corresponding list in the original list of lists.
+-- So here, where there are 3 lists in the input, the output is a list of 3-element lists. The first element of the lists is drawn from the '[1,2]' list, the second element is drawn from '[3,4]', and the third is in all cases '5'.
+-- i> sequence [[1,2],[3,4],[5]]
+-- o> [[1,3,5],[1,4,5],[2,3,5],[2,4,5]]
+-- Note: this function may be very useful in the Groningse Grid (Haskell edition) puzzle in the final programming assignment, if you choose that option.
